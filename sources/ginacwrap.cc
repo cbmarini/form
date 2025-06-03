142834133f52c64d9f4a75221af465640fdb5410
cbmarini <cbmarinissen@gmail.com>
2025-06-03 14:40:39 +0200

    feat: support imaginary parts in lin_ and hpl_ for real inputs
    Extended EvaluatePolylog, CalculateLin, and CalculateHpl to compute imaginary parts of lin_ and hpl_, lifting the -1 ≤ x ≤ 1 restriction. Input is still limited to real numbers for now.

--- sources/ginacwrap.cc
+++ sources/ginacwrap.cc
@@ -71,24 +71,16 @@ int GetLinArgument(int *indexes, int *depth, mpf_t f_out, WORD *fun) {
   	#] GetLinArgument :
   	#[ CalculateLin :
 */
-int CalculateLin(mpf_t result, int weight, mpf_t arg) {
+int CalculateLin(mpf_t resultRe, mpf_t resultIm, int weight, mpf_t arg) {
 	char *str = nullptr;
 	GiNaC::numeric argg;
-	GiNaC::ex resultg;
-	ostringstream oss;
+	GiNaC::ex resultg, resultgRe, resultgIm;
+	ostringstream oss,oss2;
 /*
 	Form measures precision in bits, GiNaC in decimal digits
 */
 	int numdigits = (AC.DefaultPrecision-AC.MaxWeight)*log10(2.0);
 	GiNaC::Digits = numdigits;
-/*
-	We only consider -1 <= arg <= 1
-	This can be changed later, but for now we don't burn ourselves 
-	by picking a branch.
-*/
-	if ( (mpf_cmp_ui(arg,1L) > 0) || (mpf_cmp_si(arg,-1L) < 0) ) {
-		return(-1);
-	}
 /*
 	In principle this is well defined, so we can always implement this case. 
 */
@@ -109,49 +101,45 @@ int CalculateLin(mpf_t result, int weight, mpf_t arg) {
     argg = GiNaC::numeric(str);
 	free(str);
 	resultg = GiNaC::evalf(GiNaC::Li(weight, argg));
+	resultgRe = GiNaC::real_part(resultg);
+	resultgIm = GiNaC::imag_part(resultg);
 /*
 	Convert back to mpf_t
 */
-	oss << resultg;
-	mpf_set_str(result, oss.str().c_str(), 10);
+	oss << resultgRe;
+	mpf_set_str(resultRe, oss.str().c_str(), 10);
+	oss2 << resultgIm;
+	mpf_set_str(resultIm, oss2.str().c_str(), 10);
 	return(0);
 }
 /*
   	#] CalculateLin :
   	#[ CalculateHpl :
 */
-int CalculateHpl(mpf_t result, int *indexes, int depth, mpf_t arg) {
+int CalculateHpl(mpf_t resultRe, mpf_t resultIm, int *indexes, int depth, mpf_t arg) {
 	int i;
 	GiNaC::lst indexesg;
 	char *str = nullptr;
 	GiNaC::numeric argg;
-	GiNaC::ex resultg;
-	ostringstream oss;
+	GiNaC::ex resultg,resultgRe, resultgIm;
+	ostringstream oss,oss2;
 /*
 	Form measures precision in bits, GiNaC in decimal digits
 */
 	int numdigits = (AC.DefaultPrecision-AC.MaxWeight)*log10(2.0);
 	GiNaC::Digits = numdigits;
 /*
-	We only consider -1 <= arg <= 1
-	This can be changed later, but for now we don't burn ourselves 
-	by picking a branch.
-*/
-	if ( (mpf_cmp_ui(arg,1L) > 0) || (mpf_cmp_si(arg,-1L) < 0) ) {
-		return(-1);
-	}
-/*
 	Trailing zeroes lead to factors of hpl_(0;x) = ln_(x)
-	So we only consider x > 0 in this case
+	So we only consider x != 0 in this case
 */
-	if ( (indexes[depth-1] == 0) && (mpf_cmp_ui(arg,0L) <= 0) ) {
+	if ( (indexes[depth-1] == 0) && (mpf_cmp_ui(arg,0L) == 0) ) {
 		return(-1);
 	}
 /*
-	Leading ones lead to factors of hpl_(1;x) = ln_(1-x)
-	So we only consider x < 1 in this case
+    Leading ones lead to factors of hpl_(1;x) = ln_(1-x) 
+	So we only consider x =! 1 in this case
 */
-	if ( (indexes[0] == 1) && (mpf_cmp_ui(arg,1L) >= 0) ) {
+	if ( (indexes[0] == 1) && (mpf_cmp_ui(arg,1L) == 0) ) {
 		return(-1);
 	}
 /*
@@ -167,15 +155,15 @@ int CalculateHpl(mpf_t result, int *indexes, int depth, mpf_t arg) {
     argg = GiNaC::numeric(str);
 	free(str);
 	resultg = GiNaC::evalf(GiNaC::H(indexesg, argg));
-/*
-	Sometimes GiNaC returns a small complex number, even if the result is real.
-*/
-	resultg = GiNaC::real_part(resultg);
+	resultgRe = GiNaC::real_part(resultg);
+	resultgIm = GiNaC::imag_part(resultg);
 /*
 	Convert back to mpf_t
 */
-	oss << resultg;
-	mpf_set_str(result, oss.str().c_str(), 10);
+	oss << resultgRe;
+	mpf_set_str(resultRe, oss.str().c_str(), 10);
+	oss2 << resultgIm;
+	mpf_set_str(resultIm, oss2.str().c_str(), 10);
 	return(0);
 }
 /*
@@ -187,6 +175,8 @@ int EvaluatePolylog(PHEAD WORD *term, WORD level, WORD par) {
 	WORD *t, *tstop, *tt, *newterm, i, depth, first = 1;
 	WORD *oldworkpointer = AT.WorkPointer, nsize, *indexes;
 	int retval;
+	mpf_t tmpRe,tmpIm, tmp;
+	mpf_init(tmpRe); mpf_init(tmpIm); mpf_init(tmp);
 
 	tstop = term + *term; tstop -= ABS(tstop[-1]);
 	if ( AT.WorkPointer < term+*term ) AT.WorkPointer = term + *term;
@@ -205,24 +195,40 @@ int EvaluatePolylog(PHEAD WORD *term, WORD level, WORD par) {
 			}
 /*
 	Step 2: evaluate
+	The real part is accumulated in aux2, the imaginary part in aux3.
 */
 			if ( first ) {
 				if ( *t == LINFUNCTION ) {
-					if (CalculateLin(aux4,indexes[0],aux1) != 0) goto nextfun;
+					if (CalculateLin(aux2,aux3,indexes[0],aux1) != 0) goto nextfun;
 				}
 				else if ( *t == HPLFUNCTION ) {
-					if (CalculateHpl(aux4,indexes,depth,aux1) != 0) goto nextfun;
+					if (CalculateHpl(aux2,aux3,indexes,depth,aux1) != 0) goto nextfun;
 				}
 				first = 0;
 			}
 			else {
 				if ( *t == LINFUNCTION ) {
-					if (CalculateLin(aux5,indexes[0],aux1) != 0) goto nextfun;
+					if (CalculateLin(aux4,aux5,indexes[0],aux1) != 0) goto nextfun;
 				}
 				else if ( *t == HPLFUNCTION ) {
-					if (CalculateHpl(aux5,indexes,depth,aux1) != 0) goto nextfun;
+					if (CalculateHpl(aux4,aux5,indexes,depth,aux1) != 0) goto nextfun;
 				}
-				mpf_mul(aux4,aux4,aux5);
+/*
+	We multiply two complex numbers:
+	(aux2+aux3*i_) * (aux4+aux5*i_) 
+			= (aux2*aux4-aux3*aux5) + (aux2*aux5+aux3*aux4)*i_
+*/
+				// Real part
+				mpf_mul(tmpRe,aux2,aux4);
+				mpf_mul(tmp,aux3,aux5);
+				mpf_sub(tmpRe,tmpRe,tmp);
+				// Imaginary part
+				mpf_mul(tmpIm,aux2,aux5);
+				mpf_mul(tmp,aux3,aux4);
+				mpf_add(tmpIm,tmpIm,tmp);
+				// We set the results again in aux2 and aux3
+				mpf_set(aux2,tmpRe);
+				mpf_set(aux3,tmpIm);
 			}
 			*t = 0;
 		}
@@ -230,7 +236,6 @@ nextfun:
 		t += t[1];
 	}
 	if ( first == 1 ) return(Generator(BHEAD term,level));
-
 /*
 	Step 3:
 	Now the regular coefficient, if it is not 1/1.
@@ -238,20 +243,24 @@ nextfun:
 */
 	nsize = term[*term-1];
 	if ( nsize < 0 ) {
-		mpf_neg(aux4,aux4);
+		mpf_neg(aux2,aux2);
+		mpf_neg(aux3,aux3);
 		nsize = -nsize;
 	}
 	if ( nsize == 3 ) {
 		if ( tstop[0] != 1 ) {
-			mpf_mul_ui(aux4,aux4,(ULONG)((UWORD)tstop[0]));
+			mpf_mul_ui(aux2,aux2,(ULONG)((UWORD)tstop[0]));
+			mpf_mul_ui(aux3,aux3,(ULONG)((UWORD)tstop[0]));
 		}
 		if ( tstop[1] != 1 ) {
-			mpf_div_ui(aux4,aux4,(ULONG)((UWORD)tstop[1]));
+			mpf_div_ui(aux2,aux2,(ULONG)((UWORD)tstop[1]));
+			mpf_div_ui(aux3,aux3,(ULONG)((UWORD)tstop[1]));
 		}
 	}
 	else {
 		RatToFloat(aux5,(UWORD *)tstop,nsize);
-		mpf_mul(aux4,aux4,aux5);
+		mpf_mul(aux2,aux2,aux5);
+		mpf_mul(aux3,aux3,aux5);
 	}
 /*
 	Now we have to locate possible other float_ functions.
@@ -260,7 +269,8 @@ nextfun:
 	while ( t < tstop ) {
 		if ( *t == FLOATFUN ) {
 			UnpackFloat(aux5,t);
-			mpf_mul(aux4,aux4,aux5);
+			mpf_mul(aux2,aux2,aux5);
+			mpf_mul(aux3,aux3,aux5);
 		}
 		t += t[1];
 	}
@@ -268,6 +278,7 @@ nextfun:
 /*
 	Now we should compose the new term in the WorkSpace.
 */
+	// Real part
 	t = term+1;
 	newterm = AT.WorkPointer;
 	tt = newterm+1;
@@ -277,13 +288,34 @@ nextfun:
 			i = t[1]; NCOPY(tt,t,i);
 		}
 	}
-	PackFloat(tt,aux4);
+	PackFloat(tt,aux2);
 	tt += tt[1];
 	*tt++ = 1; *tt++ = 1; *tt++ = 3;
 	*newterm = tt-newterm;
 	AT.WorkPointer = tt;
 	retval = Generator(BHEAD newterm,level);
+
+	// Imaginary part
+	if ( mpf_cmp_ui(aux3,0L) != 0 ) {
+	t = term+1;
+	newterm = AT.WorkPointer;
+	tt = newterm+1;
+	while ( t < tstop ) {
+		if ( *t == 0 || *t == FLOATFUN ) t += t[1];
+		else {
+			i = t[1]; NCOPY(tt,t,i);
+		}
+	}
+	*tt++ = SYMBOL; *tt++ = 4; *tt++ = ISYMBOL; *tt++ = 1;
+	PackFloat(tt,aux3);
+	tt += tt[1];
+	*tt++ = 1; *tt++ = 1; *tt++ = 3;
+	*newterm = tt-newterm;
+	AT.WorkPointer = tt;
+	retval = Generator(BHEAD newterm,level);
+	}
 	AT.WorkPointer = oldworkpointer;
+	mpf_clear(tmpRe); mpf_clear(tmpIm); mpf_clear(tmp);
 	return(retval);
 }
 /*
